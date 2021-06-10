03909db9d951d50cbc6052b50fb248267d863b71
besesito <bartekszmude@gmail.com>
2021-06-10 22:21:10 +0200

    add service models

---
+++ services/urls.py
@@ -0,0 +1,31 @@
+"""Service_Project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from .views import Customer_add, Customer_update, Customer_detail, Customer_list, Tag_add, Tag_detail
+
+app_name = 'customer'
+
+urlpatterns = [
+    #CUSTOMERS CRUD + list
+    path('', Customer_list.as_view(), name='list'),
+    path('dodaj', Customer_add.as_view(), name='add'),
+    path('<int:pk>/edytuj', Customer_update.as_view(), name='update'),
+    path('<int:pk>/detail', Customer_detail.as_view(), name='detail'),
+    #TAGS CRUD TODO
+    path('dodaj_tag', Tag_add.as_view(), name='add_tag'),
+    path('<int:pk>/detail_tag', Tag_detail.as_view(), name='tag_detail')
+]
