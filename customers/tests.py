321fe8567f24a68607d54e4f03d7adaccb96d197
besesito <bartekszmude@gmail.com>
2021-07-16 18:24:01 +0200

    customer tests

--- customers/tests.py
+++ customers/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Customer
 
 # Create your tests here.
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        self.c1 = Customer.objects.create(name="Teltonika", nip="123456546")
+        self.c2 = Customer.objects.create(name="MS-Projekt", nip="987654321")
+    def test_customer_intro(self):
+        self.assertEqual(self.c1.natural_key(), 'Teltonika')
+        self.assertEqual(self.c2.natural_key(), 'MS-Projekt')
+
+    def test_customer_nip(self):
+        self.assertEqual(self.c1.nip, "123456546")
+        self.assertEqual(self.c2.nip, "987654321")
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.c1.get_absolute_url(), "/klienci/{}".format(self.c1.id))
+        self.assertEqual(self.c2.get_absolute_url(), "/klienci/{}".format(self.c2.id))
+
+ 
+
